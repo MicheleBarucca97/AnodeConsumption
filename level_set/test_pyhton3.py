8af07b3cfe11fcca1d5dffb58efd75700c728705
MicheleBarucca97 <michelebarucca997@gmail.com>
2024-04-04 08:41:10 +0200

    Last update of the code, something still don't work

--- level_set/test_pyhton3.py
+++ level_set/test_pyhton3.py
@@ -72,22 +72,26 @@ W = fem.FunctionSpace(domain, ("Lagrange", 1))
 n = FacetNormal(domain)
 
 phi_n = fem.Function(W)  # Level set function
-# Initial condition for phi
+
+# Initial condition for phi s.t. it is negative in the anode and positive in the bath
+# The unit normal in this case (n = Grad(phi)/||Grad(phi)||) will always point inside the bath (phi >0)
 def phi_zero(x):
     y = np.ones(len(x[1]))
     for index, x_coord in enumerate(x[0]):
-        if x_coord <= 0.34 and x[1][index] < 0.5:
-            y[index] = -1
-        if x_coord <= 0.34 and x[1][index] == 0.5:
-            y[index] = 0
-        if 0.34 <= x_coord <= 0.64 and x[1][index] < 0.74:
-            y[index] = -1
-        if 0.34 <= x_coord <= 0.64 and x[1][index] == 0.74:
-            y[index] = 0
-        if x_coord >= 0.64 and x[1][index] < 0.5:
-            y[index] = -1
-        if x_coord >= 0.64 and x[1][index] == 0.5:
-            y[index] = 0
+        if x_coord < 0.35 or x_coord > 0.65:
+            if x[1][index] < 0.5:
+                y[index] = 0.5 - x[1][index]
+            if x[1][index] == 0.5:
+                y[index] = 0
+            if x[1][index] > 0.5:
+                y[index] = 0.5 - x[1][index]
+        if 0.35 <= x_coord <= 0.65:
+            if x[1][index] < 0.7:
+                y[index] = 0.7 - x[1][index]
+            if x[1][index] == 0.7:
+                y[index] = 0
+            if x[1][index] > 0.7:
+                y[index] = 0.7 - x[1][index]
     return y
 
 phi_n.interpolate(phi_zero)
@@ -101,11 +105,9 @@ xdmf_levelset.write_mesh(domain)
 
 # Define solution variable, and interpolate initial solution for visualization in Paraview
 phi_h = fem.Function(W)
-phi_h.name = "uh"
+phi_h.name = "phi_h"
 phi_h.interpolate(phi_zero)
 xdmf_levelset.write_function(phi_h, t)
-
-phi, w = ufl.TrialFunction(W), ufl.TestFunction(W)
 
 # Potential
 D = fem.FunctionSpace(domain, ("DG", 0))
@@ -141,7 +143,123 @@ phi_n_project = fem.Function(D)
 project(phi_n, phi_n_project)
 sigma.x.array[:] = conductivity_anode + (conductivity_bath - conductivity_anode)*smoothed_heaviside(phi_n_project, epsilon)
 sigma.x.scatter_forward()
+
+'''
+Variational problem and solver for the potential
+'''
 V = TrialFunction(W)
 csi = TestFunction(W)
 
+a = dot(sigma * grad(V), grad(csi)) * dx
+
+def V_exact_ufl(mode):
+    #return lambda x: mode.atan(mode.pi * x[1])
+    return lambda x: (10000 -8000 * x[1]) * x[1]
+
+def V_exact_numpy(mode):
+    #return lambda x: mode.arctan(mode.pi * x[1])
+    return lambda x: (10000 -8000 * x[1]) * x[1]
+
+V_numpy = V_exact_numpy(np)  # which will be used for interpolation
+V_ufl = V_exact_ufl(ufl)  # which will be used for defining the source term
+
+V_ex = fem.Function(W)
+V_ex.interpolate(V_numpy)
+
+x = SpatialCoordinate(domain)
+f = div(-sigma * grad(V_ufl(x)))
+g = sigma * dot(grad(V_ufl(x)), n)
+ds = ufl.Measure("ds", domain=domain) # This command or you can just import it from ufl
+L = f * csi * dx + g * csi * ds
+
+def boundary_D(x):
+    return np.logical_or(np.isclose(x[0], 0), np.isclose(x[0], 1))
+dofs_D = fem.locate_dofs_geometrical(W, boundary_D)
+
+BCs = [fem.dirichletbc(V_ex, dofs_D)]
+
+default_problem = fem.petsc.LinearProblem(a, L, bcs=BCs,
+                                petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+Vh = default_problem.solve()
+'''
+Variational problem and solver for level set function
+'''
+phi, w = ufl.TrialFunction(W), ufl.TestFunction(W)
+average_potGrad = fem.form(inner(sigma*grad(Vh), sigma*grad(Vh)) * dx)
+average = fem.assemble_scalar(average_potGrad)
+L2_average = np.sqrt(domain.comm.allreduce(average, op=MPI.SUM))
+delta = h.max()/(2*L2_average)
+theta = 0.5
+a_levelSet = (phi * (w + delta * dot(sigma * grad(Vh), grad(w))) * dx +
+              dt * dot(sigma * grad(Vh), grad(phi)) * (w + delta * dot(sigma * grad(Vh), grad(w))) * dx)
+L_levelSet = (phi_n * (w + delta * dot(sigma * grad(Vh), grad(w))) * dx -
+              dt * dot(sigma * grad(Vh), grad(phi_n)) * (w + delta * dot(sigma * grad(Vh), grad(w))) * dx)
+
+#Preparing linear algebra structures for time dependent problems.
+bilinear_form = fem.form(a_levelSet)
+linear_form = fem.form(L_levelSet)
+
+# Observe that the left hand side of the system does not change from one time step to another, thus we
+# only need to assemble it once. The right hand side, which is dependent on the previous time step u_n, has
+# to be assembled every time step.
+A = assemble_matrix(bilinear_form)
+A.assemble()
+b = create_vector(linear_form)
+
+# We create a linear algebra solver using PETSc, and assign the matrix A to the solver
+solver = PETSc.KSP().create(domain.comm)
+solver.setOperators(A)
+solver.setType(PETSc.KSP.Type.PREONLY)
+solver.getPC().setType(PETSc.PC.Type.LU)
+
+for i in range(num_steps):
+    t += dt
+
+    A.zeroEntries()
+    fem.petsc.assemble_matrix(A, bilinear_form)  # type: ignore
+    A.assemble()
+
+    # Update the right hand side reusing the initial vector
+    with b.localForm() as loc_b:
+        loc_b.set(0)
+    assemble_vector(b, linear_form)
+
+    # Apply Dirichlet boundary condition to the vector
+    apply_lifting(b, [bilinear_form], [[]])
+    b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b, [])
+
+    # Solve linear problem
+    solver.solve(b, phi_h.vector)
+    phi_h.x.scatter_forward()
+
+    # Update solution at previous time step (u_n)
+    phi_n.x.array[:] = phi_h.x.array
+
+    # Update sigma
+    project(phi_n, phi_n_project)
+    sigma.x.array[:] = conductivity_anode + (conductivity_bath - conductivity_anode) * smoothed_heaviside(phi_n_project,
+                                                                                                          epsilon)
+    sigma.x.scatter_forward()
+
+    # Calculate the new potential
+    a = dot(sigma * grad(V), grad(csi)) * dx
+    f = div(-sigma * grad(V_ufl(x)))
+    g = sigma * dot(grad(V_ufl(x)), n)
+    L = f * csi * dx + g * csi * ds
+
+    default_problem = fem.petsc.LinearProblem(a, L, bcs=BCs,
+                                              petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+    Vh = default_problem.solve()
+    Vh.x.scatter_forward()
+
+    average_potGrad = fem.form(inner(sigma * grad(Vh), sigma * grad(Vh)) * dx)
+    average = fem.assemble_scalar(average_potGrad)
+    L2_average = np.sqrt(domain.comm.allreduce(average, op=MPI.SUM))
+    delta = h.max() / (2 * L2_average)
+
+    # Write solution to file
+    xdmf_levelset.write_function(phi_h, t)
+
+xdmf_levelset.close()
 
